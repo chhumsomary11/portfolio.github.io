69585926ee9116e034ce6866c3e87b746fb9dc04
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2026-02-24 16:46:57 +0700

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import DownloadResume from "../component/DownloadResume";
 import Hero from "../component/hero";
 
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
   };
   return (
     <div className="min-h-screen  text-center py-12">
-      <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold mb-4 underline underline-offset-8 und">
+      <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold mb-4 underline underline-offset-8 ">
         Welcome to My Portfolio
       </h1>
       <div className="relative mx-auto w-76 h-76">
@@ -20,7 +21,10 @@ const Home = () => {
         />
       </div>
 
-      <p className="text-8xl text-center brandName mt-10"> Chhumsomary</p>
+      <p className="text-8xl text-center brandName mt-10 dark:text-amber-500">
+        {" "}
+        Chhumsomary
+      </p>
       <p>ICT Student </p>
       <p>English For Teaching Student</p>
 
@@ -58,6 +62,24 @@ const Home = () => {
               d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
               clip-rule="evenodd"
             />
+          </svg>
+        </a>
+        <a href="https://www.linkedin.com/in/chhumso-mary-loeung-980574268/">
+          <svg
+            class="linkedIn w-6 h-6 "
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z"
+              clip-rule="evenodd"
+            />
+            <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
           </svg>
         </a>
         <a href="">
@@ -101,9 +123,7 @@ const Home = () => {
             National AI in Education Competition. Enjoy exploring cross-cultural
             perspectives and sharing knowledge through teaching and content.
           </p>
-          <div className="bg-brown-500 border-2 border-brown-100  text-milky-500 rounded-3xl  w-48 mt-10 p-3 text-center cursor-pointer scaleTransition">
-            Download my Resume
-          </div>
+          <DownloadResume />
         </div>
         <div className="shadow-2xl bg-amber-500 p-8 rounded-4xl relative  w-80 h-88 ">
           <div className="absolute text-milky-500 bg-forest-500 dark:bg-brown-500 z-50 top-40 left-[-50px] p-5 rounded-4xl transform transition-all duration-300 hover:scale-105 hover:shadow-2xl">
