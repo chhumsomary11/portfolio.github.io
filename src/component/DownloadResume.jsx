d48bc9e8ab2ab01405144995a2bbed85b1413b48
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-6.local>
2026-03-25 14:33:40 +0700

    new

--- src/component/DownloadResume.jsx
+++ src/component/DownloadResume.jsx
@@ -1,25 +1,39 @@
+// const DownloadResume = () => {
+//   const fileUrl = "/files/CV.pdf";
+//   const handleDownload = () => {
+//     // Create a temporary link element
+//     const link = document.createElement("a");
+//     link.href = fileUrl; // Path to your resume file
+//     link.download = "Resume.pdf"; // Name of the downloaded file
+
+//     // Append the link to the body and trigger the download
+//     document.body.appendChild(link);
+//     link.click();
+
+//     // Clean up by removing the link element
+//     document.body.removeChild(link);
+//   };
+//   return (
+//     <div
+//       onClick={handleDownload}
+//       className="bg-brown-500 border-2 border-brown-100  text-milky-500 rounded-3xl  w-48 mt-10 p-3 text-center cursor-pointer scaleTransition"
+//     >
+//       Download my Resume
+//     </div>
+//   );
+// };
+
+// export default DownloadResume;
+
 const DownloadResume = () => {
-  const fileUrl = "/files/CV.pdf";
-  const handleDownload = () => {
-    // Create a temporary link element
-    const link = document.createElement("a");
-    link.href = fileUrl; // Path to your resume file
-    link.download = "Resume.pdf"; // Name of the downloaded file
-
-    // Append the link to the body and trigger the download
-    document.body.appendChild(link);
-    link.click();
-
-    // Clean up by removing the link element
-    document.body.removeChild(link);
-  };
   return (
-    <div
-      onClick={handleDownload}
-      className="bg-brown-500 border-2 border-brown-100  text-milky-500 rounded-3xl  w-48 mt-10 p-3 text-center cursor-pointer scaleTransition"
+    <a
+      href="/files/CV.pdf"
+      download="Resume.pdf"
+      className="bg-brown-500 border-2 border-brown-100 text-milky-500 rounded-3xl w-48 mt-10 p-3 text-center cursor-pointer scaleTransition block"
     >
       Download my Resume
-    </div>
+    </a>
   );
 };
 
