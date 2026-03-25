b4bc4a2b2de7a46944c52d16ebd0583d0172724c
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-6.local>
2026-03-25 14:46:45 +0700

    updated cv

--- src/component/DownloadResume.jsx
+++ src/component/DownloadResume.jsx
@@ -28,7 +28,7 @@
 const DownloadResume = () => {
   return (
     <a
-      href="/files/CV.pdf"
+      href="/files/generalCV.pdf"
       download="Resume.pdf"
       className="bg-brown-500 border-2 border-brown-100 text-milky-500 rounded-3xl w-48 mt-10 p-3 text-center cursor-pointer scaleTransition block"
     >
